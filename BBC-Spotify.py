f7513a198bd392a510d229bfaab21d932ced3130
Javier Villalba <jvillalba@samurai.ar>
2023-04-05 13:47:42 -0300

    v2.0

--- BBC-Spotify.py
+++ BBC-Spotify.py
@@ -1,6 +1,8 @@
 # Radio BBC to Spotify Playlist
 import re
 import requests
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
 url = input('BBC URL: ')
 regex = r"(https?:\/\/open.spotify.com\/(track|user|artist|album)\/[a-zA-Z0-9]+(\/playlist\/[a-zA-Z0-9]+|)|spotify:(track|user|artist|album):[a-zA-Z0-9]+(:playlist:[a-zA-Z0-9]+|))"
@@ -30,14 +32,14 @@ if len(tracks) > 0:
         track_spotify = tracks[track][0].replace(
             "https://open.spotify.com/track/", "spotify:track:")
         tracks_spotify.append(track_spotify)
-    playlist_id = input('Spotify Playlist ID: ')
+    File_Playlist = open("playlist.txt", "r")    
+    playlist_id = File_Playlist.read().splitlines()[0]
     spotify_url = "https://api.spotify.com/v1/playlists/{playlist_id}/tracks".format(
         playlist_id=playlist_id)
-    print("Spotify Endpoint: " + spotify_url)
-    OAuth_Token = input('Spotify OAuth Token: ')
-    headersAuth = {
-        'Authorization': 'Bearer ' + str(OAuth_Token),
-    }
-    response = requests.post(spotify_url, headers=headersAuth, json={
-                             'uris': tracks_spotify}, verify=True)
-    print("Resultado: " + str(response.json()))
+    File_Playlist.close()    
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="YOUR_APP_CLIENT_ID",
+                                               client_secret="YOUR_APP_CLIENT_SECRET",
+                                               redirect_uri="http://localhost:7777/callback",
+                                               scope="playlist-modify-public,playlist-modify-private"
+                                               ))
+    print(sp.playlist_add_items(playlist_id, tracks_spotify))
--- README.md
+++ README.md
@@ -6,10 +6,13 @@
 <h1 align="center">Migrar de BBC Radio-Sounds hacia Spotify</h1>
 <p align="center">
 Script en Python 3 para migrar el Tracklist de una Emisión de BBC Radio / Sounds hacia una Playlist de Spotify
+
+Biblioteca Necesaria: python3 -m pip install spotipy
+Colocar Id de Playlist en playlist.txt
 </p>
 <p align="center">
   <img src="https://raw.githubusercontent.com/jvillalba007/BBC-Spotify/main/Demo.png" />
 </p>
 <p align="center">
-OAuth Token se puede obtener desde https://developer.spotify.com/console/post-playlist-tracks/
+Apps Token se puede obtener desde https://developer.spotify.com/dashboard/
 </p>
